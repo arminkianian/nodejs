42876a5e3405cf92ca6aa51c6e2e9ad8711fb358
arminkianian <arminkia.dev@gmail.com>
2025-08-08 10:57:48 +0330

    Building a Basic Polymorphic Component

--- React and TypeScript - The Practical Guide/adv-components/src/App.tsx
+++ React and TypeScript - The Practical Guide/adv-components/src/App.tsx
@@ -1,19 +1,10 @@
 import Button from "./components/Button";
-import Input from "./components/Input";
+import Container from "./components/Container";
 
 function App() {
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
-      <p>
-        <Button>A Button</Button>
-      </p>
-      <p>
-        <Button target="_blank" href="https://google.com">
-          A Link
-        </Button>
-      </p>
+      <Container as={Button} />
     </main>
   );
 }

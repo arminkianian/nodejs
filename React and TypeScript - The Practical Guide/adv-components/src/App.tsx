25a9af2b5f1170561898ccecfd40e4bd04eb1dc9
arminkianian <arminkia.dev@gmail.com>
2025-08-14 11:29:37 +0330

    Using forwardRef with TypeScript

--- React and TypeScript - The Practical Guide/adv-components/src/App.tsx
+++ React and TypeScript - The Practical Guide/adv-components/src/App.tsx
@@ -1,10 +1,11 @@
-import Button from "./components/Button";
-import Container from "./components/Container";
+import { useRef } from "react";
+import Input from "./components/Input";
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Container as={Button}>Click Me</Container>
+      <Input label={"Test"} id={"test"} ref={inputRef} />
     </main>
   );
 }
